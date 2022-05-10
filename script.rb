5065b5aaea5dd54dfdb9a427903d0bf0f7142119
Shisui <aca.jevdjevic@gmail.com>
2022-05-10 14:35:20 +0200

    Create methods humanPlay and verify_order

--- script.rb
+++ script.rb
@@ -2,47 +2,78 @@ module Mastermind
     
     $colors = ["red", "blue", "white", "green", "black", "orange"]
 
-    
-
     $user_color_input = []
 
     $computer_colors_code = []
 
     class Game
+
         def initialize(player_1, player_2)
             @player_1 = player_1.new()
             @player_2 = player_2.new()
         end
+
         def play 
-            if $computer_colors_code == $user_color_input
-                puts "Player 1 wins"
-            else
-                puts "wait"
+            game = 0
+            while (game < 12) do
+                if $computer_colors_code == $user_color_input
+                    puts "Player 1 wins"
+                    game = 100
+                elsif game > 0 
+                   verify_order_and_color
+                   puts "hint, red pegs: #{$red_peg} white pegs: #{$white_peg}"
+                   $red_peg = 0
+                   $white_peg = 0
+                end
+                if game != 100
+                    humanPlay 
+                    game +=1
+                end
             end
         end
-        
+
+        def humanPlay 
+            puts "Guess the colors"
+            color_1 = gets.chomp
+            color_2 = gets.chomp
+            color_3 = gets.chomp
+            color_4 = gets.chomp
+
+            $user_color_input = [color_1, color_2, color_3, color_4]
+        end
+
+        def verify_order_and_color
+            $red_peg = 0
+            $white_peg = 0
+            i = 0
+            $user_color_input.each do |elem|
+                if $computer_colors_code.any?(elem) && elem != $computer_colors_code[i]
+                    $white_peg += 1
+                    i += 1
+                
+                elsif $computer_colors_code.any?(elem) && elem == $computer_colors_code[i]
+                    $red_peg += 1
+                    i += 1
+                end 
+            end
+        end
+
+       
     end
 
     class Player
-        
+       
     end
 
     class HumanPlayer < Player
-
-        puts "I'am a human i have choice between colors #{$colors}"
-        puts "Guess the colors"
-        color_1 = gets.chomp
-        color_2 = gets.chomp
-        color_3 = gets.chomp
-        color_4 = gets.chomp
-
-        $user_color_input = [color_1, color_2, color_3, color_4]
+        puts "Choice between colors #{$colors}"
     end
     class ComputerPlayer < Player
-        puts "Me computer i have choice between colors #{$colors}"
-
-        $computer_colors_code = ["red", "white", "green", "orange"]
-
+       
+        $computer_colors_code = $colors.sample(4)
+           
+        p $computer_colors_code
+       
     end
 
 end
