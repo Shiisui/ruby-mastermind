5a55d05f19da55edc9fb42f1b62f9341b395bbbc
Shisui <aca.jevdjevic@gmail.com>
2022-05-11 10:36:19 +0200

    Algorithm for codebreaking

--- script.rb
+++ script.rb
@@ -6,6 +6,11 @@ module Mastermind
 
     $code_maker = []
 
+    $round = nil
+
+    $white_data = []
+    $red_data = []
+
     class Game
         @@makeorguess = 0
         def initialize(player_1, player_2, makeorguess)
@@ -65,7 +70,17 @@ module Mastermind
 
                 $code_breaker = [color_1, color_2, color_3, color_4]
             elsif @@makeorguess == 2
-                $code_breaker = $code_maker
+                if $red_data.length != 0 || $white_data.length != 0
+                    $code_breaker << $red_data
+                    $code_breaker << $white_data
+                    $code_breaker.uniq
+                    $red_data = []
+                    $white_data = []
+                elsif $red_data.length == 0 || $white_data.length == 0
+                    $code_breaker = $colors.sample(4)
+                end
+                
+
             end
         end
 
@@ -88,17 +103,22 @@ module Mastermind
         def verify_order_and_color
             $red_peg = 0
             $white_peg = 0
+            
             i = 0
             $code_breaker.each do |elem|
                 if $code_maker.any?(elem) && elem != $code_maker[i]
                     $white_peg += 1
+                    $white_data << elem
                     i += 1
-                
+                    
                 elsif $code_maker.any?(elem) && elem == $code_maker[i]
                     $red_peg += 1
+                    $red_data << elem
                     i += 1
                 end 
             end
+            $round = 1
+           
         end
 
        
@@ -122,9 +142,11 @@ end
 include Mastermind
 
 print "Type 1 to be guesser or Type 2 to create the code: "
-
-playerOne = gets.chomp.to_i
-
+playerOne = ""
+until (playerOne == 1 || playerOne == 2)
+    playerOne = gets.chomp.to_i
+    
+end
 
 
 
